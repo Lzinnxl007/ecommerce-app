aa954d3bbc9616070a2e1fa8457c8658c6feab4f
Lzinnxl007 <leosantana.silva27@gmail.com>
2026-02-20 14:09:17 -0300

    fix: complete login route logic

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,8 +1,56 @@
-import type { NextRequest } from "next/server"
-import { NextResponse } from "next/server"
-import { Response } from "@/lib/api/Response"
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import { Response } from "@/lib/api/Response";
+import { UserService } from "@/service/user/user.service";
+import crypto from "node:crypto";
+import { User } from "@/types/user/user";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
 
 export async function POST(request: NextRequest) {
-    console.log("oi")
-    return Response("ok", 200)
+  const user = await request.json();
+
+  const { email, password } = user;
+
+  if (!email || !password) {
+    return Response("User data not provided!", 401);
+  }
+
+  const findedUser = (await UserService.GetUser(email)) as User;
+
+  if (!findedUser) {
+    return Response("User not finded!", 400);
+  }
+
+  if (findedUser.status !== "ACTIVE") {
+    return Response("User is not active!", 401);
+  }
+
+  const hash = crypto.createHash("sha256");
+  hash.update(password);
+  const hashedPassword = hash.digest("hex");
+
+  if (hashedPassword !== findedUser.password) {
+    return Response("User credentials are invalid!", 401);
+  }
+
+  const token = jwt.sign(
+    {
+      id: findedUser.id,
+      name: findedUser.name,
+      email: findedUser.email,
+      status: findedUser.status,
+    },
+    process.env.JWT_SECRET!,
+    { expiresIn: "7d" },
+  );
+
+  const cookiesStore = cookies();
+
+  cookiesStore.set({
+    name: "user_token",
+    value: token,
+    httpOnly: true,
+    secure: true,
+  });
 }
