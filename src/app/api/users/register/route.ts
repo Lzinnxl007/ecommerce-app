0592e52864a956227d144af8ce31d6f55bd5021b
Lzinnxl007 <leosantana.silva27@gmail.com>
2026-02-20 11:41:29 -0300

    fix: add email verification and convert password to hash before create user

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -1,6 +1,7 @@
 import { Response } from "@/lib/api/Response"
 import { UserService } from "@/service/user/user.service"
 import type { NextRequest } from "next/server"
+import crypto from "node:crypto"
 
 export async function POST(request: NextRequest) {
     const user = await request.json()
@@ -10,6 +11,16 @@ export async function POST(request: NextRequest) {
     if(!name || !email || !password) {
         return Response("User data not provided", 404)
     }
+
+    const findedUser = await UserService.GetUser(email)
+
+    if(findedUser) {
+        return Response("User already exist!", 401)
+    }
+
+    const hash = crypto.createHash("sha256")
+    hash.update(password)
+    user.password = hash.digest("hex")
 
     return await UserService.CreateUser(user)
 }
