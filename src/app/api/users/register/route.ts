0ddc0a0845fdc511adb62d27308ed9ba18208142
Lzinnxl007 <leosantana.silva27@gmail.com>
2026-02-20 14:08:00 -0300

    feat: verify email logic in user register

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -1,7 +1,8 @@
+import { redis } from "@/db/redis/redis"
 import { Response } from "@/lib/api/Response"
 import { UserService } from "@/service/user/user.service"
 import type { NextRequest } from "next/server"
-import crypto from "node:crypto"
+import crypto, { randomBytes } from "node:crypto"
 
 export async function POST(request: NextRequest) {
     const user = await request.json()
@@ -22,5 +23,19 @@ export async function POST(request: NextRequest) {
     hash.update(password)
     user.password = hash.digest("hex")
 
-    return await UserService.CreateUser(user)
+    const response = await UserService.CreateUser(user)
+
+    if(response?.status !== 200) {
+        return Response("Error to create user!", 500)
+    }
+
+    const verifyEmailToken = randomBytes(32).toString("hex")
+
+    await redis.set(`email-verify:${verifyEmailToken}`, email, { EX: 60 * 10 })
+
+    const url = new URL("/api/users/verify-email", request.url)
+    const params = url.searchParams
+    params.set("token", verifyEmailToken)
+
+    return Response(url, 200)
 }
