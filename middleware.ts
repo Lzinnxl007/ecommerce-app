6164fbb255c1f616472adbf04b2cec1b403ea4d4
Lzinnxl007 <leosantana.silva27@gmail.com>
2026-02-21 17:32:07 -0300

    feat: create session route and hook

--- middleware.ts
+++ middleware.ts
@@ -12,5 +12,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config: MiddlewareConfig = {
-    matcher: ["/api/users/register"]
+    matcher: ["api/auth/session"]
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+"use client"
+
+import { GetSession } from "@/hooks/getSession";
 
 export default function Home() {
+
+  const user = GetSession()
+  console.log(user)
+
   return (
     <main></main>
   );
